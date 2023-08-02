f1f941c0da47e4f280ec541d1aad2f1719179c0b
Mokshparashar <parasharm239@gmail.com>
2023-08-02 20:03:39 +0530

    completed

--- src/Items.jsx
+++ src/Items.jsx
@@ -10,6 +10,7 @@ const Items = () => {
     },
   });
   console.log(data);
+  const mainData = data.taskList;
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -17,7 +18,15 @@ const Items = () => {
   if (isError) {
     return <h3>{error.response.data}</h3>;
   }
-  const mainData = data.taskList;
+  if (mainData.length === 0) {
+    return (
+      <h3
+        style={{ textAlign: "center", marginTop: "1rem", color: "royalblue" }}
+      >
+        No item
+      </h3>
+    );
+  }
   return (
     <div className="items">
       {mainData.map((item) => {
--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,10 +1,35 @@
+import { useMutation } from "@tanstack/react-query";
+import customSetup from "./axios/utils";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 const SingleItem = ({ id, title, isDone }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customSetup.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: ({ id }) => {
+      return customSetup.delete(`/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success(`${title} deleted successfully`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    },
+  });
+
   return (
     <div className="single-item" key={id}>
       <input
         type="checkbox"
         checked={isDone}
-        onChange={() => console.log("edit task")}
+        onChange={() => editTask({ taskId: id, isDone: !isDone })}
       />
       <p
         style={{
@@ -17,7 +42,7 @@ const SingleItem = ({ id, title, isDone }) => {
       <button
         className="btn remove-btn"
         type="button"
-        onClick={() => console.log("delete task")}
+        onClick={() => deleteTask({ id })}
       >
         delete
       </button>
